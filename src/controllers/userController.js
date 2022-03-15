e6caa12ecb63f8a12eff2f536a40a6ece9dabc6e
soo <sooyoungs0120@gmail.com>
2022-03-15 21:41:21 +0900

    ♻️ Separate code: Implement router and controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,6 @@
+export const join = (req, res) => res.send("JOIN");
+export const edit = (req, res) => res.send("EDIT USER");
+export const remove = (req, res) => res.send("REMOVE USER");
+export const login = (req, res) => res.send("LOGIN");
+export const logout = (req, res) => res.send("LOOUT");
+export const see = (req, res) => res.send("SEE USER");
---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req, res) => res.send("HOME PAGE VIDEOS");
+export const see = (req, res) => res.send("SEE VIDEO")
+export const edit = (req, res) => res.send("EDIT VIDEO");
+export const search = (req, res) => res.send("SEARCH");
+export const deleteVideo = (res, req) => res.send("DELETE VIDEO");
+export const upload = (req, res) => res.send("UPLOAD");
