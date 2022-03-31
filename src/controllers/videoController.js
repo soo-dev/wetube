5b8147ea5d496dcd129ea08584084a68174df3f1
soo <sooyoungs0120@gmail.com>
2022-03-31 18:27:55 +0900

    📝 Extending templates and Setting a variable to templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,12 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    return res.send("EDIT VIDEO");
-}
+export const trending = (req, res) => res.render("home", {
+    pageTitle: "Home",
+});
+export const see = (req, res) => res.render("watch", {
+    pageTitle: "Watch",
+});
+export const edit = (req, res) => res.render("edit", {
+    pageTitle: "Edit"
+});
 export const search = (req, res) => res.send("SEARCH");
 export const deleteVideo = (req, res) => {
     return res.send("DELETE VIDEO");
